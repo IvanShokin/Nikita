440ffaa6a3a63794e483031378541ea4045e119d
Nerzhul <sinokbati@gmail.com>
2023-04-22 14:35:41 +0300

    тест

--- main_easy.py
+++ main_easy.py
@@ -8,6 +8,7 @@ game_space = []
 for _ in range(SIZE_GAME_SPACE):
     game_space.append([' ' for _ in range(SIZE_GAME_SPACE)])
 
+
 vector = 's'
 apple_x, apple_y = randint(0, SIZE_GAME_SPACE - 1), randint(0, SIZE_GAME_SPACE - 1)
 
